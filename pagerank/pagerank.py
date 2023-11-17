756224fbf407400a3ba12a0bd1ffdc95188d3c1e
Marcin <marcan.grab@gmail.com>
2023-11-17 17:57:46 +0100

    added sample_pagerank

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -50,20 +50,32 @@ def crawl(directory):
 
 def transition_model(corpus, page, damping_factor):
     probability_distribution = {}
-    
-    raise NotImplementedError
+    for key in corpus:
+        probability_distribution[key] = probability_distribution.get(key, 0) + (
+            1 - damping_factor
+        ) / len(corpus)
+        if len(corpus[page]) == 0:
+            probability_distribution[key] += damping_factor / len(corpus)
+        elif key == page:
+            for page_key in corpus[key]:
+                probability_distribution[page_key] = probability_distribution.get(
+                    page_key, 0
+                ) + damping_factor / len(corpus[key])
+    return probability_distribution
 
 
 def sample_pagerank(corpus, damping_factor, n):
-    """
-    Return PageRank values for each page by sampling `n` pages
-    according to transition model, starting with a page at random.
+    pagerank = {}
+    page = random.choice(list(corpus.keys()))
+    for i in range(n):
+        pagerank[page] = pagerank.get(page, 0) + 1
+        transition = transition_model(corpus, page, damping_factor)
+        page = random.choices(list(transition.keys()), weights=list(transition.values()), k=1)[0]
 
-    Return a dictionary where keys are page names, and values are
-    their estimated PageRank value (a value between 0 and 1). All
-    PageRank values should sum to 1.
-    """
-    raise NotImplementedError
+    for key in pagerank:
+        pagerank[key] = pagerank[key] / n
+
+    return pagerank
 
 
 def iterate_pagerank(corpus, damping_factor):
